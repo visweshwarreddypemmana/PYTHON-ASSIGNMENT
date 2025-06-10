904b58ae409cfbef93a762b25475cd7488e0c11b
PEMMANA VISWESHWAR REDDY <143106948+visweshwarreddypemmana@users.noreply.github.com>
2025-06-10 21:17:11 +0530

    Update Task_2.py

--- ASSIGNMENT_2/Task_2.py
+++ ASSIGNMENT_2/Task_2.py
@@ -1,5 +1,4 @@
-i = 1
-while i <= 50:
-    total = i+i
-    i = i + 1
-print("The sum of the numbers from 1 to 50 is:" ,total)
+total = 0
+for i in range(1, 51):
+    total += i
+print("The sum of the numbers from 1 to 50 is:", total)
